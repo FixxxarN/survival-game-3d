4edaea6d4f80a9d44765e43561b9fdcc0370fcb8
Toni <skorotoni@hotmail.com>
2020-09-21 21:15:03 +0200

    Food and prefab

---
+++ survival-game-3d/Assets/Scripts/Food Script/Coconut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coconut : MonoBehaviour
+{
+    private float _hungerIncreaseValue = 20f;
+    private float _thirstIncreasevalue = 10f;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerStats Player = other.GetComponent<PlayerStats>();
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Player.IncreaseHunger(_hungerIncreaseValue);
+                Player.IncreaseThirst(_thirstIncreasevalue);
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
