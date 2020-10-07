f1ea9d9eb1498e4e8cebe3b54172ff62c213635e
FixxxarN <tornstromandre@gmail.com>
2020-10-07 22:25:18 +0200

    Made basic ui for player stats

--- survival-game-3d/Assets/Scripts/PlayerStats.cs
+++ survival-game-3d/Assets/Scripts/PlayerStats.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float _health = 100f;
-    private float _stamina = 100f;
+    [SerializeField] private float _stamina = 100f;
     [SerializeField] private float _hunger = 100f;
     [SerializeField] private float _thirst = 100f;
 
@@ -13,6 +15,11 @@ public class PlayerStats : MonoBehaviour
     private float _maxStamina = 100f;
     private float _maxHunger = 100f;
     private float _maxThirst = 100f;
+
+    [SerializeField] private Image _healthBar;
+    [SerializeField] private Image _hungerBar;
+    [SerializeField] private Image _thirstBar;
+    [SerializeField] private Image _staminaBar;
 
     public float Health
     {
@@ -40,6 +47,19 @@ public class PlayerStats : MonoBehaviour
         InvokeRepeating("DecreaseHunger", 5f, 35f);
         InvokeRepeating("DecreaseThirst", 5f, 12f);
         InvokeRepeating("IncreaseHealth", 5f, 5f);
+    }
+
+    void Update()
+    {
+        UpdateUIStats();
+    }
+
+    private void UpdateUIStats()
+    {
+        _healthBar.rectTransform.localScale = new Vector3(_health / 100, 1, 1);
+        _hungerBar.rectTransform.localScale = new Vector3(_hunger / 100, 1, 1);
+        _thirstBar.rectTransform.localScale = new Vector3(_thirst / 100, 1, 1);
+        _staminaBar.rectTransform.localScale = new Vector3(_stamina / 100, 1, 1);
     }
 
     private void DecreaseHunger()
